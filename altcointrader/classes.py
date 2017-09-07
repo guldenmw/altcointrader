ac29ef5b3f36be301de253f0393461798c5fce64
wilhelmg <wilhelm.guldenpfennig@sunrise.co.za>
2017-09-07 20:36:19 +0200

    -added run function

--- altcointrader/classes.py
+++ altcointrader/classes.py
@@ -5,7 +5,7 @@ import logging
 from json import JSONDecodeError
 from pprint import pprint, pformat
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
@@ -20,6 +20,7 @@ class Channels(object):
         self.xrp = None
         self.zec = None
 
+        log.debug("Initializing...")
         self.update()
 
     def update(self):
@@ -29,6 +30,8 @@ class Channels(object):
         except JSONDecodeError as e:
             result = None
             log.warning("JSONDecodeError: %s" % e)
+
+        log.debug("\nresult: \n{}\n".format(pformat(result)))
 
         self.btc = result['BTC'] if result else None
         self.bcc = result['BCC'] if result else None
@@ -40,3 +43,12 @@ class Channels(object):
         self.zec = result['ZEC'] if result else None
 
         return True
+
+    def run(self):
+        while True:
+            self.update()
+            time.sleep(1)
+
+if __name__ == '__main__':
+    altws = Channels()
+    # altws.update()
